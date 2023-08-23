768ce9a3df381e73a59bbd632f13cce5d30cd000
ZIXIANG JI <jerryji040506@ucla.edu>
2023-08-22 17:58:30 -0700

    update context blind algorithm

--- context-blind.py
+++ context-blind.py
@@ -3,8 +3,8 @@ import math
 import matplotlib.pyplot as plt
 
 # initialization of settings:
-n_arms = 10
-n_theta = 10
+n_arms = 3
+n_theta = 3
 n_features = 2
 num_action_list = 4
 
@@ -100,39 +100,51 @@ def ExplorationPolicy(Z, T):
     return list_W, list_T
      
 
-def find_T_tleda(T, M):
+def find_T(T, M):
     d_telda = n_features * math.log(T * T * n_features * n_arms) * math.log(T * T/Lambda)
     # M: batch setting 
-    
     h = min(n_arms, n_features)
-    T_tleda = []  
-
-    if d_telda <= T <= d_telda * h ** (2- 2**(-M+2)):
-        r = math.ceil(T ** (1/(2- 2**(-M+2))) * d_telda ** ((1 - 2**(-M+2))/(2 - 2**(-M+2))))
+    list_T_k = []  
+    if d_telda <= T <= d_telda * h ** (2- 2**(-M+2)):        
+        r = math.ceil( T ** (1/(2- 2**(-M+2))) * d_telda ** ((1 - 2**(-M+2))/(2 - 2**(-M+2))))
         assert(r >= d_telda)
         # initialize T1, T2
-        T_tleda.append(r)
-        T_tleda.append(r)
+        list_T_k.append(r)
+        list_T_k.append(r)
         for t in range(2, M):
-            T_tleda.append(math.ceil(r * math.sqrt(T_tleda[t-1])/math.sqrt(d_telda)))
+            list_T_k.append(math.ceil(r * math.sqrt(list_T_k[t-1])/math.sqrt(d_telda)))
     elif T > d_telda * h ** (2- 2**(-M+2)): 
-        r = math.ceil(T ** (1/(2- 2**(-M+2))) * d_telda ** ((1 - 2**(-M+2))/(2 - 2**(-M+2))) * h ** ((2**(-M)-1)/(2-2**(-M+1))))
+        r = math.ceil(T ** (1/(2 - 2**(-M+1))) * d_telda ** ((1 - 2**(-M+1))/(2 - 2**(-M+1))) * h ** ((2**(-M+1)/(2-2**(-M+1)))))
         assert(r >= d_telda)
-        T_tleda.append(r)
-        T_tleda.append(r)
+        list_T_k.append(r)
+        list_T_k.append(math.ceil((r * math.sqrt(list_T_k[0]))/math.sqrt(d_telda * h)))
         for t in range(2, M):
-            T_tleda.append(math.ceil(r * math.sqrt(T_tleda[t-1])/math.sqrt(d_telda * h)))
+            list_T_k.append(math.ceil(r * math.sqrt(list_T_k[t-1])/math.sqrt(d_telda)))
 
-    return T_tleda
+    return list_T_k
+
+def find_T_tleda(T, M):
+    list_T = find_T(T, M)
+    T_delta = []
+    current_sum = 0
+    for i in range(len(list_T)):
+        current_sum += list_T[i]
+        T_delta.append(min(current_sum, T))
+    assert(len(T_delta) == len(list_T))
+    return T_delta, list_T
 
 def natural_elimination(A, delta, theta_hat):
+    # try to prevent empty set:
+    if len(A) == 1:
+        return A
     delta_inv = np.linalg.inv(delta)
     res = []
     alpha = math.sqrt(50 * math.log(n_arms * T * T * n_features))
+    # print('A', A)
+    # print('delta', delta)
+    # print('theta', theta_hat)
     for a in A: 
         for y in A:
-            if np.array_equal(a, y):
-                continue
             first_part = np.dot(a.T, theta_hat) + alpha * math.sqrt(np.dot(np.dot(a.T,delta_inv), a))
             second_part =  np.dot(y.T, theta_hat) - alpha * np.dot(np.dot(y.T,delta_inv), y)
             if first_part >= second_part:
@@ -146,16 +158,21 @@ T = 10000
 Lambda = 10/T 
 delta = Lambda * np.eye(n_features) 
 M = math.ceil(math.log(math.log(T)))
-T_tleda = find_T_tleda(T, M)
-print(T_tleda)
+print(M)
+T_tleda, list_T = find_T_tleda(T, M)
+print('list_T', list_T)
+print('T_tleda', T_tleda)
 list_A_t = []
 list_y_t = []
 list_r_t = []
-list_delta = [None] * (M+1)
+list_delta = [None] * M
 list_delta[0] = delta
 
-list_theta_hat = [None] * (M+1) 
+list_theta_hat = [None] * M 
 list_theta_hat[0] = theta_hat
+
+reg = 0
+list_reg = []
 
 for t in range(T_tleda[0]):
     #randomly chosen an action set
@@ -164,18 +181,24 @@ for t in range(T_tleda[0]):
     #we need to pass A_t as a copy, as A_t is changed inside
     pi = g_optimal_design(A_t)
     # randomly select a random arm from A_t according to g_optimal design 
+    pi = pi/sum(pi)
     selected_idx = np.random.choice(len(A_t), p=pi)
     a_t = A_t[selected_idx]
-
+   
     # calcualte reward
     noise = np.random.normal(0.0, 1.0)
     r_t = np.dot(a_t, theta_star) + noise 
+
+    optimal_reward = max([np.dot(a, theta_star) for a in A_t])
+    reg += optimal_reward - np.dot(a_t, theta_star)
+    # print(reg/math.log(T))
+    list_reg.append(reg/math.log(T))
 
     # save r_t and a_t
     list_y_t.append(a_t)
     list_r_t.append(r_t)
 
-list_delta[1] = delta + sum([np.outer(list_y_t[i], list_y_t[i]) for i in range(math.ceil(T_tleda[0]/2))])
+list_delta[1] = Lambda * np.eye(n_features)  + sum([np.outer(list_y_t[i], list_y_t[i]) for i in range(math.ceil(T_tleda[0]/2))])
 delta1_inverse = np.linalg.inv(list_delta[1])
 list_theta_hat[1] = np.dot(delta1_inverse, sum([list_r_t[i] * list_y_t[i] for i in range(math.ceil(T_tleda[0]/2))]))
 
@@ -187,69 +210,117 @@ for i in range(math.ceil(T_tleda[0]/2), T_tleda[0]):
 
 # initialzing to be pi_2
 pi_k = ExplorationPolicy(Z, T)
+print(pi_k)
+end = False 
 for k in range(1, M):
-    for t in range(T_tleda[k-1]+1, T_tleda[k-1]+T_tleda[k]+1):
+    for t in range(T_tleda[k-1]+1, T_tleda[k]+1):
         # observe X_t
         A_t = action_sets[np.random.randint(0,num_action_list)]
+        # observe X_t
+        list_A_t.append(A_t)
+        # print('A_t chosen: ', A_t)
+
         A_t_k = []
+        survivied_vector_set = []
+
+        # find intersection set  
         for i in range(1, k+1):
-            # first iteration
-            if i == 1:
-                A_t_k = natural_elimination(A_t, list_delta[i], list_theta_hat[i])
-                print('first iteration', A_t_k)
-                continue 
-            for new_arr in natural_elimination(A_t, list_delta[i], list_theta_hat[i]):
-                A_t_k = [arr for arr in A_t_k if not np.array_equal(arr, new_arr)] 
-                # # if only one arm left: 
-                # if len(A_t_k) == 1:
-                #     break
-                print('each', i, A_t_k)
+            # print(M)
+            # print('range', k)
+            # print(i, list_theta_hat[i])
+            survivied_vector_set.append(natural_elimination(A_t, list_delta[i], list_theta_hat[i]))
+        # print(survivied_vector_set)
 
-        print('res each time',  A_t_k)
+        for cur_vector in survivied_vector_set[0]:
+            # print('current vector', cur_vector)
+            count = 0 
+            for set in survivied_vector_set:
+                for vector in set: 
+                    if np.array_equal(vector, cur_vector):
+                        count += 1 
+                        break
+            # print('count', count)
+            if count == len(survivied_vector_set):
+                A_t_k.append(cur_vector)
 
-
-        list_A_t.append(A_t_k)
-
+        # print(len(A_t_k))
         # play the arm with the feature vector y_t given pi_k(A_t_k)
         list_w, list_prob = pi_k
-
         selected_idx = np.random.choice(len(list_w), p=list_prob)
         W_j_inv = np.linalg.inv(list_w[selected_idx])
-        selected_idx = np.argmax([np.dot(np.dot(x.T, W_j_inv), x) for x in A_t_k])
-
         #featured vector
+        selected_idx = np.argmax([np.dot(np.dot(x.T, W_j_inv), x) for x in A_t_k])
         y_t = A_t_k[selected_idx]
         list_y_t.append(y_t)
 
         #receive a reward 
-        r_t = np.dot(y_t, theta_star)
+        noise = np.random.normal(0.0, 1.0)
+        r_t = np.dot(y_t, theta_star) + noise
         list_r_t.append(r_t)
+
+        # calculate optimal_reward
+        optimal_reward = max([np.dot(a, theta_star) for a in A_t])
+
+        reg += optimal_reward - np.dot(y_t, theta_star)
+        # print('reg', reg/math.log(T))
+ 
+        list_reg.append(reg/math.log(T))
+        if t == T:
+            end = True
+            break
+
     
-    # print('len(list_r_t)', len(list_r_t))
-    # print('T_tleda[k-1]', T_tleda[k-1])
-    # print('T_tleda[k-1] + T_tleda[k]//2', T_tleda[k-1]  + T_tleda[k]//2)
+    # print(T_tleda)
+    # print(list_T)
+    # print(len(list_y_t))
+    # print(k)
+    # print('range', T_tleda[k-1]+1, T_tleda[k-1] + list_T[k]//2 + 1)
 
-    delta_k = delta + sum([np.outer(list_y_t[i], list_y_t[i]) for i in range(T_tleda[k-1]+1, T_tleda[k-1] + T_tleda[k]//2)])
+    # rangeT = min(T, T_tleda[k-1] + list_T[k]//2)
+    delta_k = Lambda * np.eye(n_features) + sum([np.outer(list_y_t[i], list_y_t[i]) for i in range(T_tleda[k-1]+1, T_tleda[k-1] + list_T[k]//2)])
     list_delta[k+1] = delta_k
-
     delta_k_inverse = np.linalg.inv(delta_k)
-    list_theta_hat[k+1] =  np.dot(delta_k_inverse, sum([list_r_t[i] * list_y_t[i] for i in range(T_tleda[k-1]+1, T_tleda[k-1] + T_tleda[k]//2)]))
+
+    # print('sum', sum([list_r_t[i] * list_y_t[i] for i in range(T_tleda[k-1]+1, rangeT)]))
+    list_theta_hat[k+1] =  np.dot(delta_k_inverse, sum([list_r_t[i] * list_y_t[i] for i in range(T_tleda[k-1]+1, T_tleda[k-1] + list_T[k]//2)]))
+    # print(list_theta_hat[k+1])
+
+    if end:
+        break
 
     X_k_1 = []
     for t in range(T_tleda[k-1]+1, T_tleda[k-1] + T_tleda[k]//2):
         X_k_1_t = []
+        survivied_vector_set2 = []
         for i in range(1, k+1):
-            # first iteration
-            if i == 1:
-                X_k_1_t = natural_elimination(list_A_t[t], list_delta[i], list_theta_hat[i])
-                continue 
-            for new_arr in natural_elimination(list_A_t[t], list_delta[i], list_theta_hat[i]):
-                X_k_1_t = [arr for arr in X_k_1_t if not np.array_equal(arr, new_arr)] 
+            survivied_vector_set2.append(natural_elimination(A_t, list_delta[i], list_theta_hat[i]))
+        
+        for curr_vector in survivied_vector_set2[0]:
+            count = 0
+            for set in survivied_vector_set2:
+                for vector in set:
+                    if np.array_equal(vector, curr_vector):
+                        count += 1
+            if len(survivied_vector_set2) == count:
+                X_k_1_t.append(curr_vector)
         X_k_1.append(X_k_1_t)
     
-    print(X_k_1)
 
     pi_k = ExplorationPolicy(X_k_1, T)
+    print(pi_k)
 
+
+print(reg/math.log(T))
+print(list_theta_hat)
 print(theta_star)
-print(list_theta_hat[-1])
+# time = list(range(len(list_reg)))
+# plt.plot(time, list_reg, label='Regret', color='blue')
+# plt.xlabel('Time')
+# plt.ylabel('Regret/sqrt T')
+# plt.grid(True)
+
+# # Save the figure before showing it
+# plt.savefig(f'result/Context_Blind{T}_f-{n_features}_arms-{n_arms}_nal-{num_action_list}-_theta{n_theta}.png')
+
+# Show the plot
+plt.show()
